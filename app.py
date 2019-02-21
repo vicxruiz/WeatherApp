43d3126366a8effe8a289fe7c80d9959415a6a66
victor <ruizdvictor@gmail.com>
2019-02-21 13:08:18 -0500

    hello vick

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! I think it is about that time!!!!'
+    user_age = getUserAge()
+    return user_age
 
+def getUserAge():
+
+    return age
 
 if __name__ == '__main__':
     app.run()
